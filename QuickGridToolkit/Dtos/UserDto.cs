c67df350e4408e086e67261bc00d2330b5f73ac6
Vaclav Elias <vaclavelias@gmail.com>
2023-03-18 23:50:41 +0000

    refactor: Project clean up

--- QuickGridToolkit/Dtos/UserDto.cs
+++
@@ -1,9 +0,0 @@
-namespace QuickGridToolkit.Dtos;
-
-public class UserDto
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = "n/a";
-    public string Country { get; set; } = "n/a";
-    public int Age { get; set; }
-}
