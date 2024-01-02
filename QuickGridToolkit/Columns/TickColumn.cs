22a2f8ab8f8230745dabebb1b255fe35bade96f1
Vaclav Elias <VaclavElias@gmail.com>
2024-01-02 14:17:16 +0000

    feat: Sorting corrected

--- QuickGridToolkit/Columns/TickColumn.cs
+++ QuickGridToolkit/Columns/TickColumn.cs
@@ -3,7 +3,11 @@ namespace QuickGridToolkit.Columns;
 // source https://github.com/aspnet/AspLabs/blob/main/src/QuickGrid/src/Microsoft.AspNetCore.Components.QuickGrid/Columns/PropertyColumn.cs
 public class TickColumn<TGridItem> : ColumnBase<TGridItem>
 {
-    public override GridSort<TGridItem>? SortBy { get; set; }
+    public override GridSort<TGridItem>? SortBy
+    {
+        get => _sortBuilder;
+        set => throw new NotSupportedException($"PropertyColumn generates this member internally. For custom sorting rules, see '{typeof(TemplateColumn<TGridItem>)}'.");
+    }
 
     [Parameter] public Expression<Func<TGridItem, object>> Property { get; set; } = default!;
 
@@ -15,18 +19,9 @@ public class TickColumn<TGridItem> : ColumnBase<TGridItem>
 
     protected override void CellContent(RenderTreeBuilder builder, TGridItem item)
     {
-        var isTrue = _cellTextFunc!(item) == "True";
+        var isTrue = _cellTextFunc!(item)?.ToString() == "True";
 
         builder.AddMarkupContent(2, $"<i class=\"far fa-{(isTrue ? "check" : "times")} text-{(isTrue ? "success" : "danger")}\"></i>");
-        //builder.OpenElement(0, "div");
-        //builder.AddAttribute(1, "class", "form-check");
-        //builder.AddMarkupContent(2, $"<label class=\"form-check-label\">{_cellTextFunc!(item)}</label>");
-        //builder.AddContent(3, " Hello");
-        //builder.CloseElement();
-
-        //builder.OpenComponent<Checkbox>(0);
-        //builder.AddAttribute(1, "Value", item);
-        //builder.CloseComponent();
     }
 
     protected override void OnParametersSet()
@@ -41,26 +36,37 @@ public class TickColumn<TGridItem> : ColumnBase<TGridItem>
 
             _sortBuilder = GridSort<TGridItem>.ByAscending(Property);
         }
-        if (Title is not null) return;
 
-        MemberExpression? memberExpression;
+        // It seems we don't need this below title extraciion?
 
-        if (Property.Body is UnaryExpression unaryExpression)
-            memberExpression = unaryExpression.Operand as MemberExpression;
-        else
-            memberExpression = Property.Body as MemberExpression;
+        //if (Title is not null) return;
 
-        if (memberExpression == null)
-            throw new ArgumentException($"Expression '{Property}' refers to a method, not a property.");
+        //MemberExpression? memberExpression;
 
-        if (memberExpression.Member is not PropertyInfo propertyInfo)
-            throw new ArgumentException($"Expression '{Property}' refers to a field, not a property.");
-
-        Title = propertyInfo.Name;
-
-        //if (Title is null && Property.Body is MemberExpression memberExpression)
+        //if (Property.Body is UnaryExpression unaryExpression)
         //{
-        //    Title = memberExpression.Member.Name;
+        //    memberExpression = unaryExpression.Operand as MemberExpression;
         //}
+        //else
+        //{
+        //    memberExpression = Property.Body as MemberExpression;
+        //}
+
+        //if (memberExpression == null)
+        //{
+        //    throw new ArgumentException($"Expression '{Property}' refers to a method, not a property.");
+        //}
+
+        //if (memberExpression.Member is not PropertyInfo propertyInfo)
+        //{
+        //    throw new ArgumentException($"Expression '{Property}' refers to a field, not a property.");
+        //}
+
+        //Title = propertyInfo.Name;
+
+        if (Title is null && Property.Body is MemberExpression memberExpression)
+        {
+            Title = memberExpression.Member.Name;
+        }
     }
 }
